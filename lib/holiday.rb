5670a647feac4754f0731d6e3e14aab4f89c001d
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-15 19:55:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |holiday, items| 
+holiday_hash.each do |holiday, items|
   puts "{holiday}: {items}"
 end
 
